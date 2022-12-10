c0d510696ef1c22be5b5d4d46b3fb908b83ae74a
Stefan Rappl <stefan.rappl@edu.fh-joanneum.at>
2022-12-10 19:16:17 +0100

    Day02 part 1 finisched

---
+++ Day02/day02.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+# Open a file
+file = open("input.txt", "r")
+
+sum = 0
+inputdata: list[str] = []
+
+for line in file:
+    tmp = line.rstrip().replace(' ','')
+    inputdata.append(tmp)
+
+# Close opend file
+file.close()
+
+for data in inputdata:
+    if data == 'CX' or data == 'AY' or data == 'BZ':
+        sum = sum + 6
+    elif data == 'AX' or data == 'BY' or data == 'CZ':
+        sum = sum + 3
+
+
+    if data[1] == 'X':
+        sum = sum + 1
+    elif data[1] == 'Y':
+        sum = sum + 2
+    elif data[1] == 'Z':
+        sum = sum + 3
+
+
+print(sum)
